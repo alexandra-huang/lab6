6fde22fe6250f6dc21779a8aa595594d3570df26
Alexandra Huang <a5huang@ucsd.edu>
2015-02-13 02:19:36 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,7 +26,24 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	$.get("/project/" + idNumber, addProject);
+
 	console.log("User clicked on project " + idNumber);
+}
+
+function addProject(result) {
+	console.log(result);
+
+	var project = $('#project' + result['id']);
+	var info = project.find('.details');
+
+	var details = '<span href="#" class="thumbnail">' + 
+		'<img src="' + result['image'] + '" class="detailsImage">' +
+		'<p>' + result['title'] + '</p>' + 
+		'<p><small>' + result['date'] + '</small></p>' + 
+		'<p><small>' + result['summary'] + '</small></p></span>';
+
+	info.html(details);
 }
 
 /*
@@ -35,4 +52,16 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+	$.get("/palette", addPalette);
+}
+
+function addPalette(result) {
+	var colors = result.colors.hex;
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
